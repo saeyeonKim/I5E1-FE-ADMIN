ede17486475facddfe62eb119e92aea6ae05445c
Kim JinWoo <kimjinu2540@gmail.com>
2023-07-31 18:02:30 +0900

    Init: craco 설정 중

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
-import BaseLayout from '../components/layout/BaseLayout'
+import BaseLayout from '@components/layout/BaseLayout'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
